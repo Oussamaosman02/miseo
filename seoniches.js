64c3c53f54015ed3bc9dea1a307081b305dd6fab
Oussama Osman <oussamaosman02@gmail.com>
2023-08-29 21:20:31 +0200

    Updating script

--- seoniches.js
+++ seoniches.js
@@ -1,11 +1,15 @@
 const hayNichos = (niche) => {
   const elementos = [];
   const citeElements = document.querySelectorAll("cite");
-  citeElements.forEach((ele) =>
-    elementos.push(ele.childNodes[0].textContent.includes(niche))
-  );
+  citeElements.forEach((ele) => {
+    elementos.push({
+      textIncludes: ele.childNodes[0].textContent.includes(niche),
+      node: ele,
+    });
+  });
   console.log({
-    nichos: elementos.some((elem) => elem),
-    num: elementos.filter((v) => v).length,
+    nichos: elementos.some((elem) => elem.textIncludes),
+    num: elementos.filter((v) => v.textIncludes).length,
+    elements: elementos.filter((v) => v.textIncludes),
   });
 };
